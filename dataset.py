31ff2f7b00a60263bdc761d0c52170750520c617
Ali akbar <95933132+AliakbarMzadeh@users.noreply.github.com>
2025-05-27 15:40:03 +0200

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,103 @@
+import os
+import torch
+import trimesh
+import numpy as np
+import torch.nn as nn
+import SimpleITK as sitk
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset as dataset
+from utils import get_json, get_medical_image, save_csv, get_csv, norm_zero_one
+
+
+def convert(l):
+    return list(map(float, l))
+
+
+class VesselSegmentMeshDataSet(dataset):
+    def __init__(self, json_path, indexes, isTrain=True, dire=''):
+        super(VesselSegmentMeshDataSet, self).__init__()
+        config = get_json(file=json_path)
+
+        self.isTrain = isTrain
+        self.images = []
+        self.labels = []
+        self.meshes = []
+
+        for idx in indexes:
+            items = config[idx]
+
+            for item in items:
+                image_path = item['image']
+                label_path = item['label']
+                meshs_path = item['verts']
+
+                self.images.append(os.path.join(dire, image_path))
+                self.labels.append(os.path.join(dire, label_path))
+                self.meshes.append(os.path.join(dire, meshs_path))
+
+    def __getitem__(self, index):
+        # Read raw image with SimpleITK to get metadata
+        image_sitk = sitk.ReadImage(self.images[index])
+        origin = image_sitk.GetOrigin()
+        spacing = image_sitk.GetSpacing()
+        size = image_sitk.GetSize()
+
+        # Read mesh CSV and normalize to voxel space
+        verts = np.asarray(list(map(convert, get_csv(self.meshes[index], delimiter=' '))))
+        verts = (verts - origin) / spacing
+        verts = 2. * verts / size[0] - 1.
+        verts = verts[np.random.choice(len(verts), 3000)]
+
+        # Load actual voxel array from image and label
+        image, _ = get_medical_image(self.images[index])
+        image = norm_zero_one(image, span=[-200, 400])
+        label, _ = get_medical_image(self.labels[index])
+
+        return image[np.newaxis, :, :, :], label[np.newaxis, :, :, :], verts
+
+
+
+        image = sitk.ReadImage(self.images[index])
+        verts = np.asarray(list(map(convert, get_csv(self.meshes[index], delimiter=' '))))
+        verts = (verts - image.GetOrigin()) / image.GetSpacing()
+        verts = 2. * verts / image.GetSize()[0] - 1.
+        verts = verts[np.random.choice(len(verts), 3000)]
+
+        image, _ = get_medical_image(image)
+        image = norm_zero_one(image, span=[-200, 400])
+        label, _ = get_medical_image(self.labels[index])
+
+        return image[np.newaxis, :, :, :], label[np.newaxis, :, :, :], verts
+
+    def __len__(self):
+        return len(self.images)
+
+
+def get_dataset():
+    dire = '/content/converted_dataset'
+    json_path = '/content/converted_dataset/data.json'
+
+    indexes = list(get_json(json_path).keys())
+    train_index = indexes[:-2]
+    valid_index = indexes[-2:]
+
+    train_dataset = VesselSegmentMeshDataSet(json_path, train_index, isTrain=True, dire=dire)
+    valid_dataset = VesselSegmentMeshDataSet(json_path, valid_index, isTrain=False, dire=dire)
+
+    return train_dataset, valid_dataset
+
+
+if __name__ == "__main__":
+    train_dataset, valid_dataset = get_dataset()
+
+    dataloader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+
+    for image, label, mesh in dataloader:
+        print(image.size())
+        print(label.size())
+        print(mesh.size())
+        print(mesh.max())
+        print(mesh.min())
+        print('===========================================')
+
+        exit()
